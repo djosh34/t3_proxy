7b2cb18cb4e7730296970b418709d28e40381c5f
Joshua Azimullah <j.r.azimullah@student.tudelft.nl>
2025-09-13 14:37:22 +0200

    Sat Sep 13 14:37:22 CEST 2025

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ import httpProxy from "http-proxy";
 import zlib from "zlib";
 import fs from "fs";
 
-const sslOptions = {
-  key: fs.readFileSync("./certs/key.pem"),
-  cert: fs.readFileSync("./certs/cert.pem"),
-};
-
 const BIND_ADDR = process.env.PROXY_BIND || "127.0.0.1";
 const PROXY1_PORT = process.env.PROXY1_PORT || 9222;
 const PROXY1_TARGET = process.env.PROXY1_TARGET || "https://t3.chat";
 const PROXY2_PORT = process.env.PROXY2_PORT || 9223;
-const PROXY2_TARGET =
-  process.env.PROXY2_TARGET || "https://api.sync.t3.chat";
+const PROXY2_TARGET = process.env.PROXY2_TARGET || "https://api.sync.t3.chat";
+const CERT_KEY_PEM_PATH = process.env.CERT_KEY_PEM_PATH || "./certs/key.pem";
+const CERT_PEM_PATH = process.env.CERT_PEM_PATH || "./certs/cert.pem";
+
+const sslOptions = {
+  key: fs.readFileSync(CERT_KEY_PEM_PATH),
+  cert: fs.readFileSync(CERT_PEM_PATH),
+};
+
 
 const FIND = process.env.PROXY1_FIND || "api.sync.t3.chat";
 const REPLACE = process.env.PROXY1_REPLACE || "127.0.0.1:9223";
